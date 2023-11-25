b9ba3cf328fdd2db43d267eacbc1efb80d8e1616
Yurii Khomiak <yurii.khomiak.stores@gmail.com>
2023-11-25 13:02:21 +0200

    chore: indentation

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,36 +1,36 @@
 -- NVIM API completion
 local neodev = vim.F.npcall(require, "neodev")
 if neodev then
-  neodev.setup {
-    override = function(_, library)
-      library.enabled = true
-      library.plugins = true
-    end,
-    lspconfig = true,
-    pathStrict = true,
-  }
+    neodev.setup {
+        override = function(_, library)
+            library.enabled = true
+            library.plugins = true
+        end,
+        lspconfig = true,
+        pathStrict = true,
+    }
 end
 
 local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 lsp.ensure_installed({
-  'tsserver',
-  'gopls'
+    'tsserver',
+    'gopls'
 })
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
 lsp.setup_nvim_cmp({
-  mapping = lsp.defaults.cmp_mappings({
-    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ['<Tab>'] = nil,
-    ['<S-Tab>'] = nil,
-  })
+    mapping = lsp.defaults.cmp_mappings({
+        ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ['<Tab>'] = nil,
+        ['<S-Tab>'] = nil,
+    })
 })
 
 lsp.set_preferences({
@@ -44,18 +44,18 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
@@ -78,10 +78,12 @@ lspconfig.gopls.setup({
     }
 })
 
+local go_group_id = vim.api.nvim_create_augroup("GolangLspconfig", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    vim.lsp.buf.format({async = false})
-  end
+    pattern = "*.go",
+    group = go_group_id,
+    callback = function()
+        vim.lsp.buf.format({async = false})
+    end
 })
 
