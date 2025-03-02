d94091b9e522dc830f755e798b740e35968d97c6
Yurii Khomiak <yurii.khomiak.stores@gmail.com>
2025-03-02 18:03:51 +0100

    feat: basic lsp setup + lua lsp

--- lua/yuk/plugins/lsp.lua
+++ lua/yuk/plugins/lsp.lua
@@ -1,4 +1,45 @@
+local config_keybindings = function()
+    local opts = {}
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end
+
+local config_lua = function(lspconfig)
+    -- LSP repo: https://github.com/luals/lua-language-server
+    lspconfig.lua_ls.setup {}
+end
+
 return {
-    -- TODO: LSP
-    -- TODO: Snippets (especially for markdown)
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "folke/lazydev.nvim",
+                ft = "lua", -- only load on lua files
+                opts = {
+                    library = {
+                        -- See the configuration section for more details
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            }
+        },
+        config = function()
+            local lspconfig = require('lspconfig')
+
+            config_keybindings()
+
+            config_lua(lspconfig)
+        end
+    }
 }
