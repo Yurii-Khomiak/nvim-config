3196b85d1219df3c724e5ec2f262453de3d5718d
Yurii Khomiak <yurii.khomiak.stores@gmail.com>
2025-03-02 22:37:59 +0100

    chore: disable plugin checking and other minor changes

--- lua/yuk/plugins/lsp.lua
+++ lua/yuk/plugins/lsp.lua
@@ -1,23 +1,36 @@
 local config_keybindings = function()
-    local opts = {}
+    local km = vim.keymap
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    km.set("n", "gd", function() vim.lsp.buf.definition() end, {})
+    km.set("n", "K", function() vim.lsp.buf.hover() end, {})
+    km.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, {})
+    km.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, {})
+    km.set("n", "[d", function() vim.diagnostic.goto_next() end, {})
+    km.set("n", "]d", function() vim.diagnostic.goto_prev() end, {})
+    km.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, {})
+    km.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, {})
+    km.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, {})
+    km.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, {})
 end
 
 local config_lua = function(lspconfig)
     -- LSP repo: https://github.com/luals/lua-language-server
 
     local capabilities = require('blink.cmp').get_lsp_capabilities()
-    lspconfig.lua_ls.setup { capabilities = capabilities }
+    lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+        settings = {
+            Lua = {
+                format = {
+                    enable = true,
+                    defaultConfig = {
+                        indent_style = "space",
+                        indent_size = "4",
+                    }
+                },
+            }
+        },
+    }
 end
 
 local config_perl = function(lspconfig)
