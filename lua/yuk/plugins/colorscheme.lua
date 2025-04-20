d36645c2bedc77ce02ede7f885f917e7f7aba898
Yurii Khomiak <yurii.khomiak.stores@gmail.com>
2025-04-20 23:11:57 +0200

    feat: finished my colorscheme

--- lua/yuk/plugins/colorscheme.lua
+++ lua/yuk/plugins/colorscheme.lua
@@ -1,44 +1,43 @@
-local config_rose_pine = function()
-    require("rose-pine").setup({
-        variant = "main",      -- auto, main, moon, or dawn
-        dark_variant = "main", -- main, moon, or dawn
+local config_my_rasmus = function()
+    local white = "#ffffff"
+    local gray = "#c2c0c0"
+    local green = "#64b54a"
+    local blue = "#b3cff2"
+    local cyan = "#7bb099"
+    local none = "NONE"
 
-        styles = {
-            bold = true,
-            italic = true,
-            transparency = false,
-        },
+    -- Use :Telescope highlights to browser through all groups
+    -- TS groups start with @
+    vim.g.rasmus_variant = "dark"
+    vim.g.rasmus_custom_hl_groups = {
+        Comment = { fg = green, bg = none, bold = false, italic = false },
+        DiagnosticUnnecessary = { fg = gray, bg = none, bold = false, italic = false },
+        Conditional = { fg = white, bg = none, bold = false, italic = false },
+        Keyword = { fg = white, bg = none, bold = false, italic = false },
+        Identifier = { fg = gray, bg = none },
+        Operator = { fg = gray, bg = none, bold = false, italic = false },
+        TSVariable = { fg = gray, bold = false, italic = false }, -- Any variable name that does not have another highlight.
+        Type = { fg = cyan, bold = false, italic = false },
+    }
 
-        palette = {
-            main = {
-                base = '#121111',
-            },
-        },
-    })
+    vim.g.rasmus_hl_custom_groups = function(utils)
+        utils.highlight("@variable", { fg = gray })
+        utils.highlight("@type", { fg = cyan })
+        utils.highlight("@type.builtin", { fg = cyan })
+        utils.highlight("@function", { fg = blue })
+        utils.highlight("@function.method", { fg = blue })
+    end
 
-    vim.cmd.colorscheme "rose-pine"
+    vim.cmd.colorscheme "rasmus"
 end
 
 return {
-    {
-        "rose-pine/neovim",
-        enabled = false,
-        name = "rose-pine",
-        config = config_rose_pine,
-    },
     {
         "Yurii-Khomiak/rasmus.nvim",
         -- dir = "~/myprojects/nvim/rasmus",
         enabled = true,
         priority = 1000,
-        config = function()
-            vim.opt.termguicolors = true
-
-            vim.g.rasmus_italic_comments = false
-            vim.g.rasmus_bold_keywords = true
-            vim.g.rasmus_variant = "dark"
-            vim.cmd.colorscheme "rasmus"
-        end,
+        config = config_my_rasmus,
     },
 
 }
