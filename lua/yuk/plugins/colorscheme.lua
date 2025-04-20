7bbf543378072e065ef762d93a18cbe093648c3a
Yurii Khomiak <yurii.khomiak.stores@gmail.com>
2025-04-20 19:43:34 +0200

    feat: use my forked colorscheme

--- lua/yuk/plugins/colorscheme.lua
+++ lua/yuk/plugins/colorscheme.lua
@@ -21,17 +21,24 @@ end
 
 return {
     {
-        enabled = false,
         "rose-pine/neovim",
+        enabled = false,
         name = "rose-pine",
         config = config_rose_pine,
     },
     {
+        "Yurii-Khomiak/rasmus.nvim",
+        -- dir = "~/myprojects/nvim/rasmus",
         enabled = true,
-        "kvrohit/rasmus.nvim",
         priority = 1000,
         config = function()
+            vim.opt.termguicolors = true
+
+            vim.g.rasmus_italic_comments = false
+            vim.g.rasmus_bold_keywords = true
+            vim.g.rasmus_variant = "dark"
             vim.cmd.colorscheme "rasmus"
         end,
-    }
+    },
+
 }
